20b313723e0d62618f9fe569d7a472c510038d25
Camilot1 <Semion.Kap2@gmail.com>
2024-10-20 02:59:39 +0400

    Fix GET_STATE_ADJACENT_STATES_IDS VarsScope in debug

--- HOI4ModBuilder/src/scripts/commands/functions/map/states/GetStateAdjacentStatesIdsFunc.cs
+++ HOI4ModBuilder/src/scripts/commands/functions/map/states/GetStateAdjacentStatesIdsFunc.cs
@@ -34,6 +34,7 @@ namespace HOI4ModBuilder.src.scripts.commands.functions.map.states
                 lines, ref index, indent, varsScope, args
             );
 
+            _varsScope = varsScope;
             _action = delegate ()
             {
                 var adjacentStateIds = (IListObject)ScriptParser.GetValue(
