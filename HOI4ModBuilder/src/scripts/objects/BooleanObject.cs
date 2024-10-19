f8a47d6d8c0df68cfd4438d57652ac217e510f87
Camilot1 <Semion.Kap2@gmail.com>
2024-10-19 12:59:50 +0400

    Made BOOLEAN relative object

--- HOI4ModBuilder/src/scripts/objects/BooleanObject.cs
+++ HOI4ModBuilder/src/scripts/objects/BooleanObject.cs
@@ -1,10 +1,11 @@
 ﻿using HOI4ModBuilder.src.scripts.commands.declarators;
 using HOI4ModBuilder.src.scripts.exceptions;
+using HOI4ModBuilder.src.scripts.objects.interfaces;
 using HOI4ModBuilder.src.scripts.objects.interfaces.basic;
 
 namespace HOI4ModBuilder.src.scripts.objects
 {
-    public class BooleanObject : IScriptObject, IPrimitiveObject
+    public class BooleanObject : IScriptObject, IPrimitiveObject, IRelativeObject
     {
         public bool Value { get; set; }
         public object GetValue() => Value;
@@ -28,6 +29,96 @@ namespace HOI4ModBuilder.src.scripts.objects
                 Value = booleanObject.Value;
             else
                 throw new InvalidValueTypeScriptException(lineIndex, args, value);
+        }
+
+        public bool IsGreaterThan(int lineIndex, string[] args, IRelativeObject relativeObject, BooleanObject result)
+        {
+            if (relativeObject is BooleanObject booleanObject)
+                result.Value = (Value ? 1 : 0) > (booleanObject.Value ? 1 : 0);
+            else if (relativeObject is CharObject charObject)
+                result.Value = (char)(Value ? 1 : 0) > charObject.Value;
+            else if (relativeObject is IntObject intObject)
+                result.Value = (Value ? 1 : 0) > intObject.Value;
+            else if (relativeObject is FloatObject floatObject)
+                result.Value = (float)(Value ? 1 : 0) > floatObject.Value;
+            else throw new InvalidValueTypeScriptException(lineIndex, args, relativeObject);
+
+            return result.Value;
+        }
+
+        public bool IsGreaterThanOrEquals(int lineIndex, string[] args, IRelativeObject relativeObject, BooleanObject result)
+        {
+            if (relativeObject is BooleanObject booleanObject)
+                result.Value = (Value ? 1 : 0) >= (booleanObject.Value ? 1 : 0);
+            else if (relativeObject is CharObject charObject)
+                result.Value = (char)(Value ? 1 : 0) >= charObject.Value;
+            else if (relativeObject is IntObject intObject)
+                result.Value = (Value ? 1 : 0) >= intObject.Value;
+            else if (relativeObject is FloatObject floatObject)
+                result.Value = (Value ? 1 : 0) >= floatObject.Value;
+            else throw new InvalidValueTypeScriptException(lineIndex, args, relativeObject);
+
+            return result.Value;
+        }
+
+        public bool IsLowerThan(int lineIndex, string[] args, IRelativeObject relativeObject, BooleanObject result)
+        {
+            if (relativeObject is BooleanObject booleanObject)
+                result.Value = (Value ? 1 : 0) < (booleanObject.Value ? 1 : 0);
+            else if (relativeObject is CharObject charObject)
+                result.Value = (char)(Value ? 1 : 0) < charObject.Value;
+            else if (relativeObject is IntObject intObject)
+                result.Value = (Value ? 1 : 0) < intObject.Value;
+            else if (relativeObject is FloatObject floatObject)
+                result.Value = (Value ? 1 : 0) < floatObject.Value;
+            else throw new InvalidValueTypeScriptException(lineIndex, args, relativeObject);
+
+            return result.Value;
+        }
+
+        public bool IsLowerThanOrEquals(int lineIndex, string[] args, IRelativeObject relativeObject, BooleanObject result)
+        {
+            if (relativeObject is BooleanObject booleanObject)
+                result.Value = (Value ? 1 : 0) <= (booleanObject.Value ? 1 : 0);
+            else if (relativeObject is CharObject charObject)
+                result.Value = (char)(Value ? 1 : 0) <= charObject.Value;
+            else if (relativeObject is IntObject intObject)
+                result.Value = (Value ? 1 : 0) <= intObject.Value;
+            else if (relativeObject is FloatObject floatObject)
+                result.Value = (Value ? 1 : 0) <= floatObject.Value;
+            else throw new InvalidValueTypeScriptException(lineIndex, args, relativeObject);
+
+            return result.Value;
+        }
+
+        public bool IsEquals(int lineIndex, string[] args, IRelativeObject relativeObject, BooleanObject result)
+        {
+            if (relativeObject is BooleanObject booleanObject)
+                result.Value = Value == booleanObject.Value;
+            else if (relativeObject is CharObject charObject)
+                result.Value = (char)(Value ? 1 : 0) == charObject.Value;
+            else if (relativeObject is IntObject intObject)
+                result.Value = (Value ? 1 : 0) == intObject.Value;
+            else if (relativeObject is FloatObject floatObject)
+                result.Value = (Value ? 1 : 0) == floatObject.Value;
+            else throw new InvalidValueTypeScriptException(lineIndex, args, relativeObject);
+
+            return result.Value;
+        }
+
+        public bool IsNotEquals(int lineIndex, string[] args, IRelativeObject relativeObject, BooleanObject result)
+        {
+            if (relativeObject is BooleanObject booleanObject)
+                result.Value = Value != booleanObject.Value;
+            else if (relativeObject is CharObject charObject)
+                result.Value = (char)(Value ? 1 : 0) != charObject.Value;
+            else if (relativeObject is IntObject intObject)
+                result.Value = (Value ? 1 : 0) != intObject.Value;
+            else if (relativeObject is FloatObject floatObject)
+                result.Value = (Value ? 1 : 0) != floatObject.Value;
+            else throw new InvalidValueTypeScriptException(lineIndex, args, relativeObject);
+
+            return result.Value;
         }
     }
 }
