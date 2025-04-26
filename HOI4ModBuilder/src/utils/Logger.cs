0f7c4a8dffe586e40992f055a6636152b100d3af
Camilot1 <Semion.Kap2@gmail.com>
2025-04-26 15:02:02 +0400

    Changed version to Alpha 0.2.5

--- HOI4ModBuilder/src/utils/Logger.cs
+++ HOI4ModBuilder/src/utils/Logger.cs
@@ -16,7 +16,7 @@ namespace HOI4ModBuilder.src.utils
     {
         public static readonly string logDirPath = FileManager.AssembleFolderPath(new string[] { "logs" });
         public static readonly string logFilePath = logDirPath + "latest.log";
-        public static readonly string version = "Alpha 0.2.4";
+        public static readonly string version = "Alpha 0.2.5";
 
         private static List<string> _warnings = new List<string>();
         private static List<string> _errors = new List<string>();
